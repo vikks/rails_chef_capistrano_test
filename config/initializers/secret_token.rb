0bc0801825f49d08318369c6273c721544a6a5ae
vikksr <vikks_17@yahoo.co.in>
2014-12-25 13:29:17 +0530

    capified postgres backed rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsChefCapistranoTest::Application.config.secret_key_base = 'bf06e78264d6ab2c315d88818dafe01f1dd24478cbf056180cec682ad42d4706511540e183b124b2c7ffd36fc8b5c4914049aa3506f4cfac82db842d38d0c359'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsChefCapistranoTest::Application.config.session_store :cookie_store, key: '_rails_chef_capistrano_test_session'
